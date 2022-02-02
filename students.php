a330b4d54dd782ba97829ff49d502a60f99e3b1f
E.M.C.P. Ekanayake <chamara.sab@outlook.com>
2022-02-02 13:48:38 +0530

    Navbar

---
+++ students.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+    <title>Students</title>
+</head>
+<body>
+    <!--navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">ABC</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-2">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Students</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Courses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Results</a>
+                    </li>
+                    <!--<li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>-->
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--end navbar-->
+</body>
+</html>
