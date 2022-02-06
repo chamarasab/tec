b9ee85d4f7a338b7b65e98af4bbbc3a313cf7d4a
besrilanka <hellocaspy@gmail.com>
2022-02-06 19:07:25 +0530

    Blue removed

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+<footer class="footer bg-light">
         <div class="container">
             <div class="footrtext">&copy; 2022/MIT219/Group6. All rights reserved.</div>
         </div>
